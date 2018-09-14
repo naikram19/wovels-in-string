077ac580087a26dd050e79d2d2b8cbf6da420241
Ram Naik <naikram1986@gmail.com>
2018-09-14 08:28:20 +0300

    The program recognises a single wovel in any case.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-var cats = ["SDJKSD"];
-var vowels = cats.map(function(cat1) {
+//The program recognizes a single vowel.
+
+var cats = [prompt("Enter your sentence:")];
+var vowels1 = cats.split();
+var vowels = vowels1.map(function(cat1) {
   return cat1.toLowerCase();
 });
-
-alert(vowels);
+alert(vowels.length);
